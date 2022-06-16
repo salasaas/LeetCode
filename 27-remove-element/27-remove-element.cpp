14cb11937f63d7d939d34c72bb17e70dd69bad3b
salasaas <92699191+salasaas@users.noreply.github.com>
2022-06-16 16:54:49 -0700

    Time: 3 ms (65.65%), Space: 8.9 MB (35.04%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val)
+    {
+        int k = nums.size();
+        int i = 0;
+        
+        while(i < k)
+        {
+            if(nums[i] == val)
+            {
+                nums[i] = nums[k - 1];
+                k--;
+            }
+            else
+                i++;
+        }
+        
+        return k;
+    }
+    
+};
